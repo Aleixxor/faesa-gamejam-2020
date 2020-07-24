1a83bce2fa3bc8d20fc4b2091ee8f6802217c9af
Leonardo Cassani de Amorim <leoferruge@gmail.com>
2020-07-24 15:13:46 -0300

    improved enemy ai

--- Scripts/Monster.gd
+++ Scripts/Monster.gd
@@ -8,30 +8,38 @@ var cdAttack = 5
 var currentCdAttack = 0
 var isAttacking = false
 var attackDamage = 15
+var last_position
 
 func _physics_process(delta):
-	if currentCdAttack > 0:
-		currentCdAttack -= delta
-	
-	if !isAttacking:
-		if is_on_floor():
-			direction.y = 0
-		else:
-			direction.y += 1
+	if collision_layer == 1 and collision_mask == 1:
 
-		if direction.x < 0:
-			$sprite.flip_h = false
-		elif direction.x > 0:
-			$sprite.flip_h = true
+		if currentCdAttack > 0:
+			currentCdAttack -= delta
 
-		motion = direction * speed
-		if motion.x != 0:
-			$sprite.play("run")
+		if !isAttacking:
+			if last_position == position:
+				direction.x = direction.x * (-1)
+			else:
+				last_position = position
+
+			if is_on_floor():
+				direction.y = 0
+			else:
+				direction.y += 1
+
+			if direction.x < 0:
+				$sprite.flip_h = false
+			elif direction.x > 0:
+				$sprite.flip_h = true
+
+			motion = direction * speed
+			if motion.x != 0:
+				$sprite.play("run")
+			else:
+				$sprite.play("idle")
+			move_and_slide(motion)
 		else:
-			$sprite.play("idle")
-		move_and_slide(motion)
-	else:
-		attack()
+			attack()
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
